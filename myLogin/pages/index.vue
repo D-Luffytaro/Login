ebd10cceb07625b45f82e77ce47fdd54ac5f5995
D-Luffytaro <regina_@live.fr>
2022-06-05 02:23:47 +0200

    first version

---
+++ myLogin/pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="max-w-md m-auto">
+    <form class="p-8">
+      <h1 class="text-center font-bold">Sign In</h1>
+      <div class="mt-8">
+        <label>Username</label><br>
+        <input type="text" class="form-input w-full p-1">
+      </div>
+      <div class="my-8">
+        <label>Password</label><br>
+        <input type="passworld" class="form-input w-full p-1">
+      </div>
+      <div>
+        <button class="text-white bg-dark-green hover:bg-mid-green w-full p-1 text-center">Log In</button>
+      </div>
+    </form>
+  </section>
+</template>
+
+<script></script>
+
+<style>
+</style>
